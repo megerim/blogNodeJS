7b3bf1c3b3d112e434abe549ee42c0fdd4575881
Mehmet Erim Gökhan <97033161+megerim@users.noreply.github.com>
2023-09-27 19:14:47 +0300

    added post page for id

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const ejs = require("ejs");
 const path = require("path");
 
+const Post = require("./models/Post");
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/blog-db', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {posts});
+});
+
+app.get("/add", (req, res) => {
+  res.render('add');
 });
 
 app.get("/post", (req, res) => {
@@ -18,6 +34,16 @@ app.get("/post", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render('about');
+});
+
+app.get("/posts/:id", async(req, res) => {
+  const post = await Post.findById(req.params.id)
+  res.render('post', {post});
+});
+
+app.post("/addpost", async (req, res) => {
+  await Post.create(req.body)
+  res.redirect("/");
 });
 
 const port = 3000;
